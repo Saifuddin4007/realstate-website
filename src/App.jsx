22aa08c90783ce23d2bcc9f74949f6886c6dc77f
Saifuddin Khan <saifuddinkhan4007@gmail.com>
2025-10-30 09:05:21 +0530

    Added the entry point that is App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from './components/Nav'
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    <div className='w-full overflow-hidden'>
+ 
+    <Header />
+    </div>
+  )
+}
+
+export default App
