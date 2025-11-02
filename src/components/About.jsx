4f136ce54976b5f71e07dbfd369f1f18da6ba1f4
Saifuddin Khan <saifuddinkhan4007@gmail.com>
2025-11-02 09:03:05 +0530

    Added animation on aboust section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { motion } from 'framer-motion';
 
 const About = () => {
   return (
-    <div className='flex flex-col items-center justify-center container mx-auto p-14 md:px-20 lg:px-33 w-full overflow-hidden' id='about'>
+    <motion.div 
+    initial={{opacity: 0, x: 200}}
+    transition= {{duration: 1}}
+    whileInView= {{opacity: 1, x:0}}
+    viewPort= {{once: true}}
+    className='flex flex-col items-center justify-center container mx-auto p-14 md:px-20 lg:px-33 w-full overflow-hidden' id='about'>
         <h1 className='text-2xl sm:text-4xl font-bold mb-2 '>About <span className='underline underline-offset-4 decoration-1 under font-light'>Our Business</span></h1>
         <p className='text-gray-500 text-2xl max-w-80 text-center mb-8'>Ready to buy your own House</p>
         <div className='flex flex-col md:flex-row items-center md:items-start md:gap-20'>
@@ -39,7 +45,7 @@ const About = () => {
 
         </div>
 
-    </div>
+    </motion.div>
   )
 }
 
