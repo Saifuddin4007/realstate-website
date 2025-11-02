3f5a586660a296a52b990486d0558cd2d444798e
Saifuddin Khan <saifuddinkhan4007@gmail.com>
2025-11-02 19:28:18 +0530

    updated version

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
   whileInView= {{opacity: 1, y:0}}
   viewPort= {{once: true}}
   className='container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white'>
-    <h2 className='text-5xl sm:text-6xl md:text-[90px] inline-block mx-w-3xl font-semibold pt-10'>Explpre Your Dream House</h2>
+    <h2 className='text-5xl sm:text-6xl md:text-[90px] inline-block mx-w-3xl font-semibold pt-10'>Explore Your Dream House</h2>
     <div className='flex flex-col gap-y-0.5 md:flex-row justify-center items-center gap-10'>
       <a href="#example" className='border-2 border-white bg-blue-300 p-2.5 m-2 rounded-xl'>Examples</a>
       <a href="#deal" className='border-2 border-white bg-amber-300 p-2 m-2 rounded-xl' >Deal With Us</a>
